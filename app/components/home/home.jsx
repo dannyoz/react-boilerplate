e71cfa2f46fb123cb9f2d11422123b15ef6d9eb2
Dan Osborne <dan.osborne@rocketmail.com>
2017-02-17 11:39:14 +0000

    Re-structured app

---
+++ app/components/home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let home = React.createClass({
+	render() {
+		return(
+			<div className="home">
+				<h1>React boilerplate</h1>
+			</div>
+		);
+	}
+});
+
+export default home;
